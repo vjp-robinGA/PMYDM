eb63a28d90e9fd8a17b138c9dc6bc3777636fa17
Robin <rgonzaleza32@informatica.iesvalledeljerteplasencia.es>
2025-10-23 20:53:06 +0200

    New Layouts

---
+++ UT5/Robin_Ej3_Layout3/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Robin_Ej3_Layout3"
+include(":app")
+ 
